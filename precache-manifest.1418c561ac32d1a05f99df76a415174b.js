9fe861c3dbf201b95ea85aee0e67768ded1f885b
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-08-16 20:24:06 -0500

    Updates

---
+++ precache-manifest.1418c561ac32d1a05f99df76a415174b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f110825bc314b115c9fee8d1814c909c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84417eff449cc65c75eb",
+    "url": "/static/css/main.d1282a8c.chunk.css"
+  },
+  {
+    "revision": "a6f7c3d2f6dd80cedf2f",
+    "url": "/static/js/2.6d060711.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d060711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84417eff449cc65c75eb",
+    "url": "/static/js/main.2de7826d.chunk.js"
+  },
+  {
+    "revision": "7b5d26d9812af50c231f",
+    "url": "/static/js/runtime-main.bb7c12e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
